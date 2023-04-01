3a2588d104600d46899e6a08b7e2f5c09ca65039
Usama Altaf <ualtaf238@gmail.com>
2023-04-02 03:45:58 +0500

    Hosted and stord on firebase

--- autopost.py
+++ autopost.py
@@ -3,7 +3,9 @@ import time
 from instagrapi import Client
 from PIL import Image
 import requests
-
+import json
+import firebase_admin
+from firebase_admin import credentials, db
 
 def login(username, password):
     print("Logging in...")
@@ -12,69 +14,89 @@ def login(username, password):
     print(f"Login successful for user: {username}")
     return client
 
-
-def fetch_latest_post_image(client, target_username):
-    print(f"Fetching the latest post from {target_username}...")
+def fetch_latest_post_image(client, target_username, posted_media_ids):
+    print(f"Fetching an image post from {target_username}...")
     user_id = client.user_id_from_username(target_username)
-    user_media = client.user_medias(user_id, amount=1)
-    media = user_media[0]
-    media_details = client.media_info(media.pk)
+    user_media = client.user_medias(user_id, amount=20)
 
-    if media_details.media_type == 1:  # Photo
-        image_url = media_details.thumbnail_url
-    elif media_details.media_type == 2:  # Video
-        print(f"Skipping video post from {target_username}")
-        return None
-    elif media_details.media_type == 8:  # Carousel
-        for item in media_details.carousel:
-            if item.media_type == 1:
-                image_url = item.thumbnail_url
-                break
+    for media in user_media:
+        media_id = media.pk
+        if media_id in posted_media_ids:
+            continue
+
+        media_details = client.media_info(media.pk)
+        caption = media_details.caption_text
+
+        if media_details.media_type == 1:  # Photo
+            image_url = media_details.thumbnail_url
+        elif media_details.media_type == 8:  # Carousel
+            for item in media_details.carousel:
+                if item.media_type == 1:
+                    image_url = item.thumbnail_url
+                    break
+            else:
+                continue
         else:
-            print(f"Skipping carousel post with no image media from {target_username}")
-            return None
-    else:
-        print(f"Unexpected media type from {target_username}")
-        return None
+            continue
 
-    response = requests.get(image_url)
-    image_filename = f"{target_username}_latest_post.jpg"
+        response = requests.get(image_url)
+        image_filename = f"{target_username}_latest_post.jpg"
 
-    with open(image_filename, 'wb') as f:
-        f.write(response.content)
+        with open(image_filename, 'wb') as f:
+            f.write(response.content)
 
-    print(f"Image fetched: {image_filename}")
-    return image_filename
+        print(f"Image fetched: {image_filename}")
+        return image_filename, caption
 
+    print(f"No suitable image post found for {target_username}")
+    return None, None, None
 
-
-
-def post_image(client, image_filename):
+def post_image(client, image_filename, caption):
     print(f"Posting image: {image_filename}")
-    caption = f"Posted by {client.username}"
     media = client.photo_upload(image_filename, caption)
     client.media_edit(media.pk, caption)
     print("Image posted.")
+    return media.pk
+
+# Load Firebase credentials from the JSON key file
+firebase_credentials = credentials.Certificate("service-account-key.json")
+
+# Initialize the Firebase app
+firebase_app = firebase_admin.initialize_app(firebase_credentials, {
+    'databaseURL': 'https://autopost-9e607-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+# Get a reference to the posted_media_ids node in the database
+posted_media_ids_ref = db.reference('posted_media_ids')
 
 def main():
-    username = "YOUR_OWN_USERNAME"
-    password = "YOUR_ACCOUNT_PASSWORD"
-    target_username = "TARGET_USERNAME"
-    interval = 60 * 60  # Post every hour
+    username = "midjourney.artss"
+    password = "u3990716ma"
+    target_username = "aiart.ly"
+    interval = 60 * 1  # Post every hour
+
 
     client = login(username, password)
 
+    # Load posted media IDs from Firebase Realtime Database or create an empty list
+    posted_media_ids = posted_media_ids_ref.get() or []
+
     while True:
         try:
-            image_filename = fetch_latest_post_image(client, target_username)
-            post_image(client, image_filename)
-            os.remove(image_filename)  # Clean up the downloaded image file
+            image_filename, caption = fetch_latest_post_image(client, target_username, posted_media_ids)
+            if image_filename and caption:
+                posted_media_id = post_image(client, image_filename, caption)
+                os.remove(image_filename)  # Clean up the downloaded image file
+
+                # Save the posted media ID to the Firebase Realtime Database
+                posted_media_ids.append(posted_media_id)
+                posted_media_ids_ref.set(posted_media_ids)
+
             print(f"Waiting {interval} seconds before the next post...")
             time.sleep(interval)
         except Exception as e:
             print(f"Error: {e}")
             time.sleep(60)  # Wait a minute before retrying in case of errors
-
 
 if __name__ == "__main__":
     main()
