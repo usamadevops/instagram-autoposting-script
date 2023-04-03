d53b16162e90b672210865849edf13baf9f6ffef
Usama Altaf <ualtaf238@gmail.com>
2023-04-03 23:04:11 +0500

    env getting

--- autopost.py
+++ autopost.py
@@ -98,15 +98,15 @@ def clip_upload(client, path: Path, caption: str, thumbnail: Path, usertags: Lis
 # Load Firebase credentials from the JSON key file
 firebase_credentials = credentials.Certificate({
     "type": "service_account",
-    "project_id":  os.environ["PROJECT_ID"],
-    "private_key_id":  os.environ["PRIVATE_KEY_ID"],
-    "private_key":  os.environ["PRIVATE_KEY"].replace("\\n", "\n") ,
-    "client_email": os.environ["CLIENT_EMAIL"],
-    "client_id":os.environ["CLIENT_ID"] ,
-    "auth_uri":  os.environ["AUTH_URI"],
-    "token_uri":  os.environ["TOKEN_URI"],
-    "auth_provider_x509_cert_url":  os.environ["AUTH_PROVIDER_X509_CERT_URL"],
-    "client_x509_cert_url":  os.environ["CLIENT_X509_CERT_URL"],
+    "project_id":  os.environ.get("PROJECT_ID"),
+    "private_key_id":  os.environ.get("PRIVATE_KEY_ID"),
+    "private_key":  os.environ.get("PRIVATE_KEY").replace("\\n", "\n") ,
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id":os.environ.get("CLIENT_ID") ,
+    "auth_uri":  os.environ.get("AUTH_URI"),
+    "token_uri":  os.environ.get("TOKEN_URI"),
+    "auth_provider_x509_cert_url":  os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url":  os.environ.get("CLIENT_X509_CERT_URL"),
 })
 
 # Initialize the Firebase app
