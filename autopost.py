afaf398e2721f63cd4fdfad7251ae7a5e7a3181c
Usama Altaf <ualtaf238@gmail.com>
2023-04-04 14:34:46 +0500

    updated Code

--- autopost.py
+++ autopost.py
@@ -100,7 +100,7 @@ firebase_credentials = credentials.Certificate({
     "type": "service_account",
     "project_id":  os.environ.get("PROJECT_ID"),
     "private_key_id":  os.environ.get("PRIVATE_KEY_ID"),
-    "private_key":  os.environ.get("PRIVATE_KEY").replace("\\n", "\n") ,
+    "private_key":  os.environ.get("PRIVATE_KEY"),
     "client_email": os.environ.get("CLIENT_EMAIL"),
     "client_id":os.environ.get("CLIENT_ID") ,
     "auth_uri":  os.environ.get("AUTH_URI"),
